0a80da7c0aff5db0c220716db521cc918693ad05
xbxfelipe98 <falves1988@gmail.com>
2020-06-17 19:55:57 -0300

    Segundo commit stream

--- src/main/java/one/digitalinovationone/stream/Programa.java
+++ src/main/java/one/digitalinovationone/stream/Programa.java
@@ -35,5 +35,48 @@ public class Programa
         //retorna uma nova coleção, com nomes concatenados  a quantidade de letras de cada nome
         System.out.println("Retorna uma nova coleção com quantidade de letras: "+ estudantes.stream().map(estudante ->
                 estudante.concat(" - ").concat(String.valueOf(estudante.length()))).collect(Collectors.toList()));
+
+        //limita elementos
+        System.out.println("Retorna 3 primeiros elementos: " +estudantes.stream().limit(3).collect(Collectors.toList()));
+
+        //Exibe cada elemento no console e depois retorna a mesma coleçãp
+        System.out.println("Retorna os elementos: " +estudantes.stream().peek(System.out::println).collect(Collectors.toList()));
+
+
+        //Exibe cada elemento sem retornar
+        System.out.println("Retorna elementos");
+        estudantes.stream().forEach(System.out::println);
+
+        //retorna true se todos os elementos possuem a letra w no nome
+        System.out.println("Todos os elemento tem o w no nome: " +estudantes.stream().allMatch((elemento ->
+                elemento.contains("w"))));
+
+        //retorna true se algum elemento possuir a letra a no nome
+        System.out.println("Algum elemento contem a letra a? " +estudantes.stream().anyMatch((elemento ->
+                elemento.contains("a") )));
+
+            //retorna true se nenhum elemento possuir a letra a no nome
+        System.out.println("Nao tem nenhum elemento contem a letra a? " +estudantes.stream().noneMatch((elemento ->
+                elemento.contains("a") )));
+
+
+        //retorna o primeiro elemento
+        System.out.println("Primeiro elemento: ");
+        estudantes.stream().findFirst().ifPresent(System.out::println);
+
+        //exemplo de operação encadeada
+
+        System.out.println("Operação encadeada:");
+        System.out.println(estudantes.stream().peek(System.out::println).map(estudante ->
+                estudante.concat(" - ").concat(String.valueOf(estudante.length()))).peek(System.out::println).
+                //filter((estudante)-> estudante.toLowerCase().contains("r")).collect(Collectors.toList()));
+                //collect(Collectors.toList()));
+                //collect(Collectors.joining(",")));
+                //collect(Collectors.toSet()));
+                collect(Collectors.groupingBy((estudante)->estudante.substring(estudante.indexOf("-")+1))));
+
+
+
+
     }
 }
